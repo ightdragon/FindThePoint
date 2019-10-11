f0ff32ee6f4d106fcc3533c1cd1a819ce6c2e933
Tejaswi Kulkarni <tejaswi.05@gmail.com>
2019-10-11 15:05:17 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,31 @@
+
+function myFiddle(){
+    
+    var x = event.pageX;
+    var y = event.pageY;
+    
+    
+    
+    var cords = "X: " + x + " Y: " + y ;
+    
+    document.getElementById("values").innerHTML = cords;
+    var data = getDate();
+    var datavar = data*10;
+
+    document.getElementById("checkthis").innerHTML = "Find this: " + datavar;
+    
+    if(x === datavar || y === datavar){
+            document.getElementById("changethis").innerHTML = "Found me!";
+        }
+    
+}
+
+function getDate(){
+    var date = new Date();
+    var seconds = date.getSeconds();
+    return seconds;
+}
+
+function emptyString(event){
+    document.getElementById("changethis").innerHTML="";
+}
